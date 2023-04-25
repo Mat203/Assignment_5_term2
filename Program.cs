bdc175c626d5e67558ada346eedf0615868da572
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-04-25 20:24:02 +0300

    Prepare part

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿static void Main()
+{
+    Console.Write("Write a sentence: ");
+    string input = Console.ReadLine();
+    for (int i = 0; i < input.Length; i++)
+    {
+        char s = input[i];
+        if (s == '.' || s == ',' || s == ';' || s == ':' || s == '-' || s == '!' || s == '?')
+        {
+            input = input.Remove(i, 1);
+            i--;
+        }
+    }
+
+    string[] words = input.Split(" ");
+
+    string[] wordList = File.ReadAllLines("words.txt");
+    List<string> typosMistakes = new List<string>();
+    
+    foreach (string word in words)
+    {
+            if (!wordList.Contains(word.ToLower()))
+            {
+                typosMistakes.Add(word);
+            }
+    }
+
+    if ( typosMistakes.Count > 0)
+    {
+        Console.Write("Looks like you have typos in next words: ");
+        Console.WriteLine(string.Join(", ", typosMistakes));
+    }
+    
+}
+
+Main();
